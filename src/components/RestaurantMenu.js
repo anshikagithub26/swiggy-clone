6282523e3fff3e8983e4cd8e97f08625502e0639
anshikagithub26 <sharmaanshika10793@gmail.com>
2024-11-10 06:41:01 +0530

    card is designed menu card

--- src/components/RestaurantMenu.js
+++ src/components/RestaurantMenu.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
+import { Link } from 'react-router-dom'
 //hi.hostinger.in for website building
 function RestaurantMenu() {
   const {id} = useParams();
@@ -8,11 +9,17 @@ let mainId = id.split("-").at(-1)
       const [menuData, setMenuData] = useState([ ])
       const [discountData, setDiscountData] = useState([ ])
       const [resInfo, setResInfo] = useState([ ])
+
+    console.log(resInfo);
+    
+
    async function fetchMenu() {
       let data = await fetch(`https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=30.7333148&lng=76.7794179&restaurantId=${mainId}&catalog_qa=undefined&submitAction=ENTER`);
       let res = await data.json();
-      console.log(res?.data?.cards);
-      // setMenuData(res?.data?.cards[2]?.card?.card?.info)
+      // console.log(res?.data?.cards[4]?.groupedCard?.cardGroupMap?.REGULAR?.cards[1]?.card?.card);
+      setResInfo(res?.data?.cards[2]?.card?.card?.info)
+      setDiscountData(res?.data?.cards[3]?.card?.card?.gridElements?.infoWithStyle?.offer)
+      setMenuData(res?.data?.cards[4]?.groupedCard?.cardGroupMap?.REGULAR?.cards[1]?.card?.card)
    }
 
   useEffect(() => {
@@ -20,10 +27,40 @@ let mainId = id.split("-").at(-1)
   },[])
   
   return (
-    <div className='font-bold font-2xl font-serif'>
-       RestaurantMenu : Anshika ---- {mainId} {menuData}
+    <div className='w-full'>
+     <div className='w-[800px] mx-auto pt-10'>
+      <p className='text-[12px] text-slate-500'> <Link to={"/"}> <span className='hover:text-slate-700 hover:cursor-pointer'>Home</span> </Link>/ <Link to={"/"}><span className='hover:text-slate-700 hover:cursor-pointer'>{resInfo.city}</span></Link> / <span className='text-slate-700'>{resInfo.name}</span></p>
+     <h1 className='font-bold pt-6 text-2xl'>{resInfo.name}</h1>
+     <div className='w-full h-[160px] bg-gradient-to-t px-4 pb-4 from-slate-300/70  mt-3 rounded-[30px]'>
+         <div className='w-full h-full rounded-[30px] bg-white border border-slate-200/70  '>
+
+         <div className='p-4 w-full'>
+        <div className='flex item-centre gap-1 font-semibold'>
+         <i className="fi fi-ss-circle-star mt-1 text-green-600 text-lg " ></i>
+         <span>{resInfo.avgRating}</span>
+         <span>({resInfo.totalRatingsString})</span>
+        <span>{resInfo.costForTwoMessage}</span>
+        </div>
+        <p className='underline font-semibold text-orange-600'>{resInfo?.cuisines?.join(" , ")}</p>
+        <div className='flex gap-2'>
+          <div className='w-[7px] flex flex-col justify-center items-center mt-3'>
+            <div className='w-[7px] h-[7px] bg-slate-400 rounded-full'> </div>
+            <div className='w-[1.5px] h-[25px] bg-slate-400 rounded-full'></div>
+            <div className='w-[7px] h-[7px] bg-slate-400 rounded-full'> </div>
+          </div>
+          <div className='flex flex-col gap-1 font-semibold text-sm mt-2'>
+            <p>Outlet<span className='text-gray-400 font-semibold'>{resInfo.locality}</span></p> 
+            <p>{resInfo.sla?.slaString}</p>
+          </div>
+          </div>
+          {/* <hr/> */}
+        </div>
+         </div>
+     </div>
+     </div>
        
     </div>
+
   )
 }
 
